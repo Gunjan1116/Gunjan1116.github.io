4b45f074c66203eba14754570a38202d0cfa92ae
Gunjan1116 <kumargunjan1116@gmail.com>
2023-04-04 15:36:57 +0530

    added add event listener in resume button-2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.getElementById('resume-link-1').addEventListener("click",()=>{
     window.open("https://drive.google.com/file/d/1aF_pPDvSQYfG2PSfxGCRkD2T4sPbCHSE/view?usp=sharing", "_blank");
   })
   
-  document.getElementById('resume-link-2').addEventListener("click",()=>{
+  document.getElementById('resume-button-2').addEventListener("click",()=>{
     // console.log("OPENinig.....")
     window.open("https://drive.google.com/file/d/1aF_pPDvSQYfG2PSfxGCRkD2T4sPbCHSE/view?usp=sharing", "_blank");
   })
