c18d3c0eb244e010afe8413da2e630e60ab76edc
Gunjan1116 <kumargunjan1116@gmail.com>
2024-07-25 16:47:16 +0530

    added updated resume

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function closemenu(){
 
 document.getElementById('resume-link-1').addEventListener("click",()=>{
     // console.log("OPENinig.....")
-    window.open("https://drive.google.com/file/d/1aF_pPDvSQYfG2PSfxGCRkD2T4sPbCHSE/view?usp=sharing", "_blank");
+    window.open("https://drive.google.com/file/d/153kDeE-KN2KyVP28FvdoXaHa_YJtrUPP/view?usp=sharing", "_blank");
   })
   
   document.getElementById('resume-link-2').addEventListener("click",()=>{
     // console.log("OPENinig.....")
-    window.open("https://drive.google.com/file/d/1aF_pPDvSQYfG2PSfxGCRkD2T4sPbCHSE/view?usp=sharing", "_blank");
+    window.open("https://drive.google.com/file/d/153kDeE-KN2KyVP28FvdoXaHa_YJtrUPP/view?usp=sharing", "_blank");
   })
